55a47333d97b1ac8966b7e5808c318523a6527ed
GitHub Action <action@github.com>
2021-05-23 11:28:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/skrtks/norminette/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/skrtks/norminette/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.skrtks.norminette.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skrtks.norminette.MyBundle
 
 class MyApplicationService {
 
